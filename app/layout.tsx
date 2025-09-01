8bbda9484ae640bd204af4a8808854ad06f720fb
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-01 22:30:19 +0700

    Fix typo in metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "SmaartFaith AI",
+  title: "SmartFaith AI",
   description: "Kumpulan Gateway AI Islami",
 };
 
